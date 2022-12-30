63a0ddbd96d331bba689b1ab735a1e9b24b7557c
drewrych <drewrych.sol@gmail.com>
2022-12-29 21:25:51 -0600

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../app.css";
+  </script>
+  
+  <slot />
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
