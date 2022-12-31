56f47436d661db072a44f181a2d357e3c90c0ee5
idrewlong <idrewlong@gmail.com>
2022-12-31 15:10:42 -0600

    added basic nav component

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,19 +1,46 @@
 
-  <nav class="nav">
-    <div>
-      <a href="/">
-        <!-- adding <a href="/" will compose the image to function as a home link -->
-        <img src="/media/logo.jpg" class="nav-logo" alt="" />
-        <!-- image on top of nav -->
-      </a>
-    </div>
+<script>
+
+
+
+  const links = [
+    {
+      href: "/a",
+      label: "Home"
+    },
+    {
+      href: "/b",
+      label: "About"
+    },
+    {
+      href: "/c",
+      label: "Portfolio"
+    },
+    {
+      href: "/d",
+      label: "Experience"
+    },
+    {
+      href: "/e",
+      label: "Contact"
+    },
+  ];
+  </script>
   
-    <div class="text-2xl">
-      <div>
-        <a href="/" class="nav-link"> Home</a>
+  
+  
+    <nav class="flex justify-between items-center w-full h-20 px-4 bg-gray-800  text-white">
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <a href="/">
+          <!-- adding <a href="/" will compose the image to function as a home link -->
+          <img src="" class="nav-logo" alt="" />
+          <!-- image on top of nav -->
+        </a>
       </div>
-      <div>
-        <a href="/about" class="nav-link">About</a>
-      </div>
-    </div>
-  </nav>
+    
+      <ul class= "flex">
+        {#each links as { href, label}}
+        <li class="px-4 cursor-pointer font-medium text-gray-500 hover:scale-105 duration-200">
+          <a {href}>{label}</a>{/each}
+      </ul>
+    </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script>
-    import Fa from 'svelte-fa'
-    import nav from '../lib/nav.svelte'
+    
 </script>
 
 <main>
-    <nav />
+    
 </main>
