261166f78cea7e8b65f303a424b69c696e5c8a09
idrewlong <idrewlong@gmail.com>
2023-02-03 23:13:54 -0600

    added edge

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,8 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ edge: true })
+		
 	},
 	preprocess: vitePreprocess()
 };
